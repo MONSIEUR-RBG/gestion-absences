c7bbdc74d663fca6fb6e397526b22f94db852ec3
MONSIEUR-RBG <eyramrizbangou@gmail.com>
2025-11-15 16:42:46 +0100

    Initial commit

---
+++ src/main/java/com/formatec/gestionabsences/GestionAbsencesApplication.java
@@ -0,0 +1,13 @@
+package com.formatec.gestionabsences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAbsencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAbsencesApplication.class, args);
+	}
+
+}
